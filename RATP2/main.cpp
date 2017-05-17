76b32a67580a104766d7dadce75ae4bb7ad589c9
brouwa <benoit.guillrd@polytechnique.edu>
2017-05-17 14:27:59 +0200

    Initial Commit

---
+++ RATP2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RATP2
+//
+//  Created by Benoît Guillard on 17/05/2017.
+//  Copyright © 2017 Benoît Guillard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
