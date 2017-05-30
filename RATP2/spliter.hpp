7f20ce0567a42b2294adb68611be7ff99f0a30bf
leflemluc <leflemluc@gmail.com>
2017-05-30 16:12:43 +0200

    ff

---
+++ RATP2/spliter.hpp
@@ -0,0 +1,14 @@
+//
+//  spliter.hpp
+//  RATP2
+//
+//  Created by luc leflem on 5/30/17.
+//  Copyright © 2017 Benoît Guillard. All rights reserved.
+//
+
+#ifndef spliter_hpp
+#define spliter_hpp
+
+#include <stdio.h>
+
+#endif /* spliter_hpp */
